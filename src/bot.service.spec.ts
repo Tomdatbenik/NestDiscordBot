b34ac17cd7570d49b7ca2dcf9c319d05e3514158
Tomdatbenik <tomvankaathoven@gmail.com>
2022-01-10 10:46:11 +0100

    test tests

---
+++ src/bot.service.spec.ts
@@ -0,0 +1,27 @@
+import { BotService } from "./bot.service";
+
+describe('botservice test', () => {
+  let service: BotService;
+
+  beforeEach(() => {
+    service = new BotService();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of cats', async () => {
+      const result = 'Hello World! you send: test';
+      jest.spyOn(service, 'getHello').mockImplementation(async () => result);
+
+      expect(await service.getHello('test')).toBe(result);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of cats', async () => {
+      const result = 'Hello World! you send: test';
+      jest.spyOn(service, 'getHello').mockImplementation(async () => result);
+
+      expect(await service.getHello('test')).toBe('Anus');
+    });
+  });
+});
